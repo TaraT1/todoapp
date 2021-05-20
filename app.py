93f609a70660d1c2d227b5af47fc775b66b69ae1
Tara Thierry <tarathierry@gmail.com>
2021-05-20 11:33:08 -0500

    Initial commit for remote repo

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template
-from sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-db.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tarat@localhost:5432/todoapp'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
-class Todo(db.Model)
+class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer(), primary_key=True)
     description = db.Column(db.String(), nullable=False)
 
 def __repr__(self):
-    return f'<Todo {self.id} {self.description}'>
+    return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all()
+    return render_template('index.html', data=Todo.query.all())
 
