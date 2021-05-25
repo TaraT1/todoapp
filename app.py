95522e8e3460e64797b576fb78fe8595288b3cd7
Tara Thierry <tarathierry@gmail.com>
2021-05-24 21:47:25 -0500

    Implement async views

--- app.py
+++ app.py
@@ -13,22 +13,19 @@ class Todo(db.Model):
 def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
-db.create_all()
+    #db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    #Modify for json: description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
-    
-    '''
     return jsonify({
         'description': todo.description
     })
-    '''
-
+    
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all())
